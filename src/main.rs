321c188dc9466b649e54c3d317362c49ade14c9b
HimanshusinghRaghav <hsinghraghav1@gmail.com>
2023-06-30 16:11:29 +0530

    add:-run video in loop

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
 mod gst_pipeline;
 
 #[derive(Debug, Deserialize, Serialize)]
 struct StreamRequest {
     input_url: String,
     output_urls: Vec<String>,
+    loopTimes: u32,
 
 }
 
@@ -13,15 +15,28 @@ async fn stream_handler(data: web::Json<StreamRequest>) -> impl Responder {
     // Extract input URL and output URLs from the request
     let input_url = &data.input_url;
     let output_urls = &data.output_urls;
+    let loopTimes = data.loopTimes;
+    let mut remaining_loops = loopTimes;
 
+    // Start the streaming pipeline
+    while remaining_loops > 0 {
+        gst_pipeline::streaming_pipeline(input_url, output_urls);
+        remaining_loops -= 1;
+    }
 
-     // Start the streaming pipeline
-	 // Start the streaming pipeline
-	 let result =  gst_pipeline::streaming_pipeline(input_url , output_urls) ;
+    // Prepare the JSON response
+    let response_data: Value = json!({
+        "success": true,
+        "message": "Streaming completed successfully",
+        "loopCount": loopTimes - remaining_loops,
+    });
 
-    // result.into();
+    println!("loop {}" , response_data);
+    // Convert the JSON object to a string
+    let response_body = serde_json::to_string(&response_data).unwrap();
 
-    HttpResponse::Ok().body("Streaming started successfully")
+    // Send the response with the JSON body
+    HttpResponse::Ok().body(response_body)
 }
 
 async fn index() -> impl Responder {
